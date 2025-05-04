3cef1b3318a6e6d88836f031c975e475e1b070df
aksdeo5 <akshaydeogaonkar2@gmail.com>
2025-05-04 12:11:46 +0530

    Handling Mutation Errors

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -26,27 +26,32 @@ const TodoForm = () => {
 	const ref = useRef<HTMLInputElement>(null);
 
 	return (
-		<form
-			className="row mb-3"
-			onSubmit={(event) => {
-				event.preventDefault();
+		<>
+			{addTodo.error && (
+				<div className="alert alert-danger">{addTodo.error.message}</div>
+			)}
+			<form
+				className="row mb-3"
+				onSubmit={(event) => {
+					event.preventDefault();
 
-				if (ref.current?.value)
-					addTodo.mutate({
-						id: 0,
-						title: ref.current?.value,
-						completed: false,
-						userId: 1,
-					});
-			}}
-		>
-			<div className="col">
-				<input ref={ref} type="text" className="form-control" />
-			</div>
-			<div className="col">
-				<button className="btn btn-primary">Add</button>
-			</div>
-		</form>
+					if (ref.current?.value)
+						addTodo.mutate({
+							id: 0,
+							title: ref.current?.value,
+							completed: false,
+							userId: 1,
+						});
+				}}
+			>
+				<div className="col">
+					<input ref={ref} type="text" className="form-control" />
+				</div>
+				<div className="col">
+					<button className="btn btn-primary">Add</button>
+				</div>
+			</form>
+		</>
 	);
 };
 
