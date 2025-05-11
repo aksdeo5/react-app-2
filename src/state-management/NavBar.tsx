e7e9ed758a7807361bcd4875adbf06cd913c6800
aksdeo5 <akshaydeogaonkar2@gmail.com>
2025-05-11 11:44:20 +0530

    Preventing Unnecessary Renders with Selectors

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -2,7 +2,9 @@ import { LoginStatus } from "./auth";
 import useCounterStore from "./counter/store";
 
 const NavBar = () => {
-	const { counter } = useCounterStore();
+	const counter = useCounterStore((s) => s.counter);
+
+	console.log("NavBar Rendered...");
 
 	return (
 		<nav className="navbar d-flex justify-content-between">
