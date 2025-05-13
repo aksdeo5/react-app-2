8b9324a8fb82362e13b157424f691f5a3681d62c
aksdeo5 <akshaydeogaonkar2@gmail.com>
2025-05-13 10:59:42 +0530

    Resetting project to valid state

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -4,8 +4,6 @@ import useCounterStore from "./counter/store";
 const NavBar = () => {
 	const counter = useCounterStore((s) => s.counter);
 
-	console.log("NavBar Rendered...");
-
 	return (
 		<nav className="navbar d-flex justify-content-between">
 			<span className="badge text-bg-secondary">{counter}</span>
